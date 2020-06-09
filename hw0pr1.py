840f8854c0d4faaf9f18f9c9347246e800ba3a22
Cristian R-C <31358896+Dabbingonmyhaters@users.noreply.github.com>
2020-06-08 21:08:24 -0700

    hw0pr1.py
    New File

---
+++ hw0pr1.py
@@ -0,0 +1,22 @@
+#Cristian Rosales-Cardenas
+#CS5
+#Day 0 (06/08/20)
+#Prof Dodds
+
+from math import *
+print('Zero is ', 4+4-4-4)
+print('One is ', 44/44)
+print('Twelve is',sqrt(4)+(4)+(4)+sqrt(4))
+print('Three is', (factorial(4)+factorial(4))/(4*4))
+print('Four is', (factorial(4)-4-4)/4)
+print('Sixteen is', 4*4/4*4)
+print('Eighteen is', (factorial(4)+factorial(4)+factorial(4))/(4))
+print('Fifteen is', 44/4+4)
+print('Ten is', 44/(4+0.4))
+print('Eleven is', 44/(sqrt(4)+sqrt(4)))
+print('Eight is', 4**(4/4)+4)
+print('Fourteen is',(4)+(4)+(4)+sqrt(4))
+print('Eight is ', (factorial(4)-(4*4)))
+print('Two is', (factorial(4)-(4*4))/4)
+print('Twenty is', (factorial(4)-(4*4))/.4)
+print('Six is', (factorial(4)+factorial(4))/(4+4))
