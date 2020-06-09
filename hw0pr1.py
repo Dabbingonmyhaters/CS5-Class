2aec545a049df9d518b203e7d98ce43f194f8ef7
Cristian R-C <31358896+Dabbingonmyhaters@users.noreply.github.com>
2020-06-08 21:17:31 -0700

    now includes hw0pr2.py

--- hw0pr1.py
+++ hw0pr1.py
@@ -20,3 +20,30 @@ print('Eight is ', (factorial(4)-(4*4)))
 print('Two is', (factorial(4)-(4*4))/4)
 print('Twenty is', (factorial(4)-(4*4))/.4)
 print('Six is', (factorial(4)+factorial(4))/(4+4))
+
+
+# coding: utf-8
+#
+# hw0pr2a.py
+#
+
+import random          # imports the library named random
+
+def rps():
+    """ this plays a game of rock-paper-scissors
+        (or a variant of that game)
+        arguments: no arguments    (prompted text doesn't count as an argument)
+        results: no results        (printing doesn't count as a result)
+    """
+    user = input("Choose your weapon: ")
+    comp = random.choice(['rock','paper','scissors'])
+    print()
+
+    print('The user (you)   chose', user)
+    print('The computer (I) chose', comp)
+    print()
+
+    if user == 'rock':
+        print('Ha! I really chose paper--I WIN!')
+
+    print("Better luck next time...")
